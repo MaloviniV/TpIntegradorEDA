da58b6b1a276b674a92bedb7f56e0db8adc75d89
Victor Malovini <malovinivjd@hotmail.com>
2024-10-28 16:25:54 -0300

    actualizacion 5.0
    ingreso automatico en busqueda

--- src/trabajofinaleda/TrabajoFinalEDA.java
+++ src/trabajofinaleda/TrabajoFinalEDA.java
@@ -1,5 +1,7 @@
 package trabajofinaleda;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /*TRABAJO FINAL EDA-2024, ASISTENTE DE COMPRAS.
@@ -13,6 +15,7 @@ public class TrabajoFinalEDA {
         Scanner entrada = new Scanner(System.in);
         entrada.useDelimiter("\n");      //cambio el delimitador del SCANNER para que tome toda la frase
         Nodo nodo;
+        int resp;
         
         ArbolBinario arbol = new ArbolBinario();
         
@@ -31,13 +34,23 @@ public class TrabajoFinalEDA {
                     
                     //NAVEGO ENTRE LAS RAMAS CREADAS
                     arbol.buscarProducto();
-                    int resp;
-                    String valor;
                     
-                    do{
+                    ingresarProducto(arbol);
+                    break;
+
+                case 2:
+                    System.out.println("El equipo de Electrónica que esta pensando...");
+                    arbol.inorden();
+                    break;
+                case 3:
+                    arbol.getFila().clear();
+                    nodo = arbol.buscarProducto();
+                    if(nodo == null || nodo.getValor().contains("?")){
+                        System.out.println("El producto no fue encontrado");
+                        
                         while (true) {
                             System.out.println("-------------------------------------------------------------");
-                            System.out.println("|            ¿Desea ingresar otra subcategoria?             |".toUpperCase());
+                            System.out.println("|            ¿Desea ingresar el producto que buscaba?             |".toUpperCase());
                             System.out.println("|                  * Ingrese una opción *                   |");
                             System.out.println("| 1 - SI                                                    |");
                             System.out.println("| 2 - NO                                                    |");
@@ -56,43 +69,11 @@ public class TrabajoFinalEDA {
                             break;
                         }
                         if(resp==1){
-                            System.out.println("Ingrese la subcategoria sin signos:");
-                            valor = entrada.next();
-                            valor = "¿"+valor+"?";
-                            //arbol.getFila().add(1);
-                        }else{
-                            System.out.println("Ingrese el producto:");
-                            valor = entrada.next();
+                            System.out.println(arbol.getFila());
+                            mostrarCamino(arbol);
+                            System.out.println(arbol.getFila());
+                            ingresarProducto(arbol);
                         }
-                        System.out.println(arbol.getFila());
-                        arbol.insertar(valor);
-                        System.out.println(arbol.getFila());
-                    }while(resp==1);
-//                    do{
-//                        System.out.println("¿Desea ingresar otra subcategoria?");
-//                        resp = entrada.next();
-//                        if(resp.equalsIgnoreCase("S")){
-//                            System.out.println("Ingrese la subcategoria sin signos:");
-//                            valor = entrada.next();
-//                            valor = "¿"+valor+"?";
-//                        }else{
-//                            System.out.println("Ingrese el producto:");
-//                            valor = entrada.next();
-//                        }
-//                        arbol.insertar(valor);
-//                    }while(resp.equalsIgnoreCase("S"));
-                    arbol.getFila().clear();
-                    break;
-
-                case 2:
-                    System.out.println("El equipo de Electrónica que esta pensando...");
-                    arbol.inorden();
-                    break;
-                case 3:
-                    nodo = arbol.buscarProducto();
-                    if(nodo == null || nodo.getValor().contains("?")){
-                        System.out.println("El producto no fue encontrado");
-                        
                     }else{
                         System.out.println("El producto es: " + nodo.getValor());
                         arbol.getFila().clear();
@@ -122,5 +103,62 @@ public class TrabajoFinalEDA {
             System.out.println("| 4 - Salir                                                 |");
             System.out.println("-------------------------------------------------------------");
             System.out.println("");
+    }
+    
+    private static void ingresarProducto(ArbolBinario arbol) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useDelimiter("\n");      //cambio el delimitador del SCANNER para que tome toda la frase
+        int resp;
+        String valor;
+        do{
+            while (true) {
+                System.out.println("-------------------------------------------------------------");
+                System.out.println("|            ¿Desea ingresar otra subcategoria?             |".toUpperCase());
+                System.out.println("|                  * Ingrese una opción *                   |");
+                System.out.println("| 1 - SI                                                    |");
+                System.out.println("| 2 - NO                                                    |");
+                System.out.println("-------------------------------------------------------------");
+
+                if(!entrada.hasNextInt()){
+                    System.out.println("**** Error! Caracter no válido ****");
+                    entrada.next();
+                    continue;
+                }
+                resp = entrada.nextInt();
+                if(!(resp==1 || resp==2)){
+                    System.out.println("**** Error! Opción incorrecta ****");
+                    continue;
+                }
+                break;
+            }
+            if(resp==1){
+                System.out.println("Ingrese la subcategoria sin signos:");
+                valor = entrada.next();
+                valor = "¿"+valor+"?";
+            }else{
+                System.out.println("Ingrese el producto:");
+                valor = entrada.next();
+            }
+            System.out.println(arbol.getFila());
+            arbol.insertar(valor);
+        }while(resp==1);
+        arbol.getFila().clear();
+    }
+
+    private static void mostrarCamino(ArbolBinario arbol) {
+        Queue<Integer> filaAux = new LinkedList<>();
+        filaAux.addAll(arbol.getFila());
+        Nodo nodo = arbol.getRaiz();
+        System.out.println("CAMINO:");
+        System.out.print(nodo.getValor()+" --> ");
+        while(filaAux.size()>1){
+            if(filaAux.poll()==1){
+                nodo = nodo.getDer();
+            }else{
+                nodo = nodo.getIzq();                
+            }
+            System.out.print(nodo.getValor()+" --> ");
+        }
+        System.out.println("");
     }
 }
